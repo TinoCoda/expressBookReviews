197746049bfd9c418ee582c3e231d01149729074
Destin B.Moanda <moanda@live.fr>
2025-06-27 15:42:54 +0200

    complete tasks 10 to 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,24 +23,24 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here
   return res.status(200).json({data: books, message: "Book list retrieved successfully"});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   const book = books[isbn];
   if (!book) {
     return res.status(404).json({message: "Book not found"});
   }
-  return res.status(200).json({message:  "Book not found", data: book });
+  return res.status(200).json({message:  "Book found", data: book });
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
   //Write your code here
   const author = req.params.author;
   const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase().includes(author.toLowerCase()) );
@@ -64,7 +64,7 @@ public_users.get('/title/:title',function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn',async function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   const book = books[isbn];
